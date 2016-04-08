26adb1903705a185dc616f81784574052be93d52
Dan Rabinowitz <danr@sanbornmediafactory.com>
2016-04-07 21:30:35 -0400

    importing text

--- KmeansMapper.java
+++ KmeansMapper.java
@@ -11,6 +11,7 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.CollectiveMapper;
 import edu.iu.harp.array.ArrPartition;
 import edu.iu.harp.array.ArrTable;
